ff649d1d57dcecbae2a72b1fac7a29b4d01312e2
Peter <peterjbark@gmail.com>
2023-05-29 12:27:11 -0700

    Wild Wild Life answered

--- lab-answers.js
+++ lab-answers.js
@@ -20,21 +20,33 @@
 // Fizz Buzz
 /////////////
 
-for (let i=1; i<=100; i++) {
-    if (i%3 === 0 && i%5 === 0){
-        console.log("FizzBuzz")
-      } else if(i%3 === 0 ){
-            console.log("Fizz")
-        } else if (i%5 === 0) {
-            console.log("Buzz") 
-        }  else {
-            console.log(i);
-         }
-       }
+// for (let i=1; i<=100; i++) {
+//     if (i%3 === 0 && i%5 === 0){
+//         console.log("FizzBuzz")
+//       } else if(i%3 === 0 ){
+//             console.log("Fizz")
+//         } else if (i%5 === 0) {
+//             console.log("Buzz") 
+//         }  else {
+//             console.log(i);
+//          }
+//        }
     
 /////////////////
 // Wild Wild Life
 /////////////////
+
+const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"];
+const sharky = ["Sharky", "shark", 20, "Left Coast"];
+const plantee = ["Plantee", "plant",  5000 , "Mordor"];
+const porgee = ["Porgee", "Porg", 186, "Ahch-To"];
+const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"];
+wolfy.shift();
+wolfy.unshift("Gameboy");
+wolfy.pop();
+wolfy.push("Gotham City");
+dart.push("Hawkins");
+plantee[2]=5001;
 
 ////////////////////////////
 // Yell at the Ninja Turtles
