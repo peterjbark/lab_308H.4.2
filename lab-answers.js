d8986f9cf270788f16cac151af145983c9bf9c0d
Peter <peterjbark@gmail.com>
2023-05-29 20:59:28 -0700

    Kristyn's lost  shoe found

--- lab-answers.js
+++ lab-answers.js
@@ -116,19 +116,49 @@
 // Find the Median
 //////////////////
 
-const nums = [14,11,16,15,13,16,15,17,19,11,12,14,19,11,15,17,11,18,12,17,12,71,18,15,12];
-nums.sort();
-let medInt = Math.floor(nums.length/2);
-console.log(nums[medInt]);
+// const nums = [14,11,16,15,13,16,15,17,19,11,12,14,19,11,15,17,11,18,12,17,12,71,18,15,12];
+// nums.sort();
+// let medInt = Math.floor(nums.length/2);
+// console.log(nums[medInt]);
 
 ///////////////////
 // Hungry for More?
 ///////////////////
-
+const kristynsCloset = [
+    "left shoe",
+    "cowboy boots",
+    "right sock",
+    "Per Scholas hoodie",
+    "green pants",
+    "yellow knit hat",
+    "marshmallow peeps"
+  ];
+  const thomsCloset = [
+    [
+      // These are Thom's shirts
+      "grey button-up",
+      "dark grey button-up",
+      "light blue button-up",
+      "blue button-up",
+    ],[
+      // These are Thom's pants
+      "grey jeans",
+      "jeans",
+      "PJs"
+    ],[
+      // Thom's accessories
+      "wool mittens",
+      "wool scarf",
+      "raybans"
+    ]
+  ];
 ///////////////
 // Alien Attire
 ///////////////
 
+let kristynsShoe = kristynsCloset.splice(0,1);
+thomsCloset[2].push(kristynsShoe);
+console.log(thomsCloset[2]);
 //////////////
 // Dress Us Up
 //////////////
